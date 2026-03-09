642f095d5317b9fa043582c3e89f573c17bbad80
risalahlele87-coder <risalahleles87@gmail.com>
2026-03-09 11:03:56 +0700

    nestedif 5

--- PRAKTIKUM-ONLINE/nested_if.cpp
+++ PRAKTIKUM-ONLINE/nested_if.cpp
@@ -19,4 +19,15 @@ int main()
     float nilBI, nilMT, rerata;
     string status;
 
-   
+    cout << "Masukkan nilai Bahasa Inggris = ";
+    cin >> nilBI;
+    cout << "Masukkan nilai Matematika = ";
+    cin >> nilMT;
+
+    // rerata = (nilBI+nilMT)/2;
+    // if (rerata >= 60)
+    //     status = "Lolos";
+    // else
+    //     status = "Gagal";
+
+    
